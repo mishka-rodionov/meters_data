aa8d63a2646c94b05cdbbd2f227a5a06f8e92119
Михаил Родионов <rodionov.mikhail.a@gmail.com>
2022-03-26 15:01:16 +0400

    buikdSrc directory and kotlin gradle files has been created

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,39 @@
+object ConfigData {
+    const val compileSdkVersion = 31
+    const val buildToolsVersion = "30.0.3"
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 31
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
+
+object Versions {
+    const val gradlePlugin = "4.2.1"
+    const val kotlin = "1.5.0"
+    const val timber = "4.7.1"
+    const val appCompat = "1.3.0"
+    const val material = "1.3.0"
+    const val constraintLayout = "1.1.3"
+    const val jUnit = "4.12"
+}
+
+/**
+ * To define plugins
+ */
+object BuildPlugins {
+    val android by lazy { "com.android.tools.build:gradle:${Versions.gradlePlugin}" }
+    val kotlin by lazy { "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}" }
+}
+
+/**
+ * To define dependencies
+ */
+object Deps {
+    val appCompat by lazy { "androidx.appcompat:appcompat:${Versions.appCompat}" }
+    val timber by lazy { "com.jakewharton.timber:timber:${Versions.timber}" }
+    val kotlin by lazy { "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}" }
+    val materialDesign by lazy { "com.google.android.material:material:${Versions.material}" }
+    val constraintLayout by lazy { "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}" }
+    val junit by lazy { "junit:junit:${Versions.jUnit}" }
+
+}
