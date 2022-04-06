190d1d354d3ef2306c930bdca14ca4ff573eff9a
Михаил Родионов <rodionov.mikhail.a@gmail.com>
2022-04-06 15:39:08 +0400

    fragment personal information xml layout has been created

--- feature/profile/src/main/java/com/rodionov/profile/presentation/presonal_info/PersonalInformationFragment.kt
+++ feature/profile/src/main/java/com/rodionov/profile/presentation/presonal_info/PersonalInformationFragment.kt
@@ -1,6 +1,7 @@
 package com.rodionov.profile.presentation.presonal_info
 
 import androidx.fragment.app.Fragment
+import com.rodionov.profile.R
 
-class PersonalInformationFragment: Fragment() {
+class PersonalInformationFragment: Fragment(R.layout.fragment_personal_information) {
 }
