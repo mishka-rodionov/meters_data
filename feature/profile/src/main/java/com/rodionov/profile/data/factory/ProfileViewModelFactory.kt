766d25cad5e0f95a661a0d7324450df6a05e2604
mishka_rodionov <rodionov.mikhail.a@gmail.com>
2022-04-12 22:48:16 +0400

    changes for personal info fragment and view model has been added

--- feature/profile/src/main/java/com/rodionov/profile/data/factory/ProfileViewModelFactory.kt
+++ feature/profile/src/main/java/com/rodionov/profile/data/factory/ProfileViewModelFactory.kt
@@ -3,6 +3,7 @@ package com.rodionov.profile.data.factory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.rodionov.profile.domain.repository.ProfileRepository
+import com.rodionov.profile.presentation.presonal_info.PersonalInformationViewModel
 import com.rodionov.profile.presentation.profile.ProfileViewModel
 import javax.inject.Inject
 import javax.inject.Provider
@@ -15,6 +16,7 @@ class ProfileViewModelFactory @Inject constructor(
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return when (modelClass) {
             ProfileViewModel::class.java -> ProfileViewModel(profileRepository)
+            PersonalInformationViewModel::class.java -> PersonalInformationViewModel(profileRepository)
             else -> throw IllegalArgumentException("Unknown ViewModel class")
         } as T
 //        return viewModelProviders[modelClass]?.get() as T
--- feature/profile/src/main/java/com/rodionov/profile/presentation/presonal_info/PersonalInformationFragment.kt
+++ feature/profile/src/main/java/com/rodionov/profile/presentation/presonal_info/PersonalInformationFragment.kt
@@ -1,7 +1,41 @@
 package com.rodionov.profile.presentation.presonal_info
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.rodionov.domain.models.User
 import com.rodionov.profile.R
+import com.rodionov.profile.data.factory.ProfileViewModelFactory
+import com.rodionov.profile.databinding.FragmentPersonalInformationBinding
+import dagger.Lazy
+import javax.inject.Inject
 
-class PersonalInformationFragment: Fragment(R.layout.fragment_personal_information) {
+class PersonalInformationFragment : Fragment(R.layout.fragment_personal_information) {
+
+    @Inject
+    lateinit var viewModelFactory: Lazy<ProfileViewModelFactory>
+
+    private val binding: FragmentPersonalInformationBinding by viewBinding(
+        FragmentPersonalInformationBinding::bind
+    )
+
+    private val viewModel: PersonalInformationViewModel by viewModels { viewModelFactory.get() }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSaveUser.setOnClickListener {
+            viewModel.setUser(
+                User(
+                    id = "",
+                    firstName = binding.etPersonalInfoFirstName.text.toString(),
+                    lastName = binding.etPersonalInfoLastName.text.toString(),
+                    phone = binding.etPersonalInfoPhone.text.toString(),
+                    email = ""
+                )
+            )
+        }
+    }
+
 }
