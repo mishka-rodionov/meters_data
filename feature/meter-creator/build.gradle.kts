f61ffa55c2bb84bf09b4ab0944cb3cbf922cd483
mishka_rodionov <rodionov.mikhail.a@gmail.com>
2022-04-20 23:24:28 +0400

    start creating of meter creator layout

--- feature/meter-creator/build.gradle.kts
+++ feature/meter-creator/build.gradle.kts
@@ -1,17 +1,19 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
-    compileSdk = 31
+    compileSdk = ConfigData.compileSdkVersion
 
     defaultConfig {
-        minSdk = 21
-        targetSdk = 31
+        minSdk = ConfigData.minSdkVersion
+        targetSdk = ConfigData.targetSdkVersion
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
+        viewBinding.isEnabled = true
     }
 
     buildTypes {
@@ -34,6 +36,11 @@ android {
 
 dependencies {
 
+    implementation(project(":core:resources"))
+    implementation(project(":core:domain"))
+    implementation(project(":core:database"))
+    implementation(project(":core:base"))
+
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("com.google.android.material:material:1.5.0")
@@ -42,4 +49,19 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+    implementation(Deps.lifecycle_runtime)
+
+    implementation(Deps.dagger)
+    kapt(Deps.daggerCompiler)
+
+    implementation(Deps.navigation_fragment)
+    implementation(Deps.navigation_fragment_ktx)
+    implementation(Deps.navigation_ui_ktx)
+
+    implementation(Deps.room_runtime)
+    implementation(Deps.room_ktx)
+    kapt(Deps.room_compiler)
+
+    implementation(Deps.viewBindingDelegate)
 }
--- feature/meter-creator/src/main/java/com/rodionov/meter_creator/presentation/meter_creator/MeterCreatorFragment.kt
+++ feature/meter-creator/src/main/java/com/rodionov/meter_creator/presentation/meter_creator/MeterCreatorFragment.kt
@@ -1,4 +1,10 @@
 package com.rodionov.meter_creator.presentation.meter_creator
 
-class MeterCreatorFragment {
+import com.rodionov.base.platform.BaseFragment
+import com.rodionov.meter_creator.R
+
+class MeterCreatorFragment: BaseFragment(R.layout.fragment_meter_creator) {
+
+
+
 }
