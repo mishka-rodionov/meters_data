9e2ac23043acf74f1aec368d3757d72dcc18e23f
Михаил Родионов <rodionov.mikhail.a@gmail.com>
2022-05-05 16:51:20 +0400

    updated material design version, added theme parameter for fragment meter creator

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -12,7 +12,7 @@ object Versions {
     const val kotlin = "1.5.0"
     const val timber = "4.7.1"
     const val appCompat = "1.3.0"
-    const val material = "1.2.0"
+    const val material = "1.6.0"
     const val constraintLayout = "1.1.3"
     const val jUnit = "4.12"
     const val dagger = "2.41"
