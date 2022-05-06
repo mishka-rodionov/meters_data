7c339f20b3da431fbb0f5686b16a8e7e6e9d0752
Михаил Родионов <rodionov.mikhail.a@gmail.com>
2022-05-06 17:59:10 +0400

    added toolbar in main activity

--- app/src/main/java/com/rodionov/meters_data/presentation/main/MainActivity.kt
+++ app/src/main/java/com/rodionov/meters_data/presentation/main/MainActivity.kt
@@ -6,8 +6,11 @@ import android.text.Spanned
 import android.text.style.ForegroundColorSpan
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.bold
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -21,12 +24,22 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val controller = binding.mainContainer.findNavController()
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
         val navController = navHostFragment.navController
 
+        initToolbar(navController)
         binding.mainBottomNavigation
             .setupWithNavController(navController)
     }
+
+    private fun initToolbar(navController: NavController) {
+        setSupportActionBar(binding.mainToolbar)
+        setupActionBarWithNavController(navController = navController, AppBarConfiguration(navGraph = navController.graph, drawerLayout = null))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.mainContainer).navigateUp()
+    }
+
 }
