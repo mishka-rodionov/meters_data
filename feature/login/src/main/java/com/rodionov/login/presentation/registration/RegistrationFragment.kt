9774e0fcdedf6121db279ce1e55f719c1e07e80b
Михаил Родионов <rodionov.mikhail.a@gmail.com>
2022-07-10 13:44:17 +0400

    created registration fragment

---
+++ feature/login/src/main/java/com/rodionov/login/presentation/registration/RegistrationFragment.kt
@@ -0,0 +1,8 @@
+package com.rodionov.login.presentation.registration
+
+import com.rodionov.base.platform.BaseFragment
+import com.rodionov.login.R
+
+class RegistrationFragment: BaseFragment(R.layout.fragment_registration) {
+
+}
