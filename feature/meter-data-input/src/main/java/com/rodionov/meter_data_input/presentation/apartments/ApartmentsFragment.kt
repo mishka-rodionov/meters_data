a61011bf6e11842320603f4f231d657bf6146626
mishka_rodionov <rodionov.mikhail.a@gmail.com>
2022-08-16 22:58:31 +0400

    created apartments and meters fragment, create apartments layout

---
+++ feature/meter-data-input/src/main/java/com/rodionov/meter_data_input/presentation/apartments/ApartmentsFragment.kt
@@ -0,0 +1,6 @@
+package com.rodionov.meter_data_input.presentation.apartments
+
+import androidx.fragment.app.Fragment
+
+class ApartmentsFragment: Fragment() {
+}
---
+++ feature/meter-data-input/src/main/java/com/rodionov/meter_data_input/presentation/meters/MetersFragment.kt
@@ -0,0 +1,6 @@
+package com.rodionov.meter_data_input.presentation.meters
+
+import androidx.fragment.app.Fragment
+
+class MetersFragment: Fragment() {
+}
